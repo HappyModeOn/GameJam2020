55b0ba08cbc207b5135484d6e58b789d5709ac71
Tinnapop Sonporm <tinnapop.s@fairplaystudios.net>
2020-04-24 06:34:29 +0700

    Init Git Import BGM/SFX Import 2DProcamera

---
+++ Assets/Scripts/SimplePooling.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimplePooling : MonoBehaviour {
+
+
+    public GameObject[] pool;
+    public int poolID = 0;
+
+    public void Clear()
+    {
+        for (int i = 0; i < pool.Length; i++)
+        {
+            if (pool[i] != null)
+            {
+                pool[i].SetActive(false);
+            }
+            
+        }
+        poolID = 0;
+    }
+    public void Show(Vector3 position)
+    {
+        
+        pool[poolID].transform.position = position;
+
+        pool[poolID].SetActive(true);
+        poolID++;
+
+        if (poolID >= pool.Length)
+        {
+            poolID = 0;
+        }
+    }
+
+}
