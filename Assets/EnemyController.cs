054294a41ddbe911a785dd2743e003c2954ea25a
Tinnapop Sonporm <tinnapop.s@fairplaystudios.net>
2020-04-24 12:52:52 +0700

    Animation

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private EnemyFloating ef;
+    public float speed = 3;
+    // Start is called before the first frame update
+    public bool isMelee = false;
+
+    private Rigidbody rb;
+    void Start()
+    {
+        ef = transform.root.GetComponent<EnemyFloating>();
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rb.MovePosition(Vector3.forward * speed * Time.deltaTime);
+        
+        if (ef.reachTarget)
+        {
+            if (isMelee)
+            {
+                if (ef.lenNumber == EnemyFloating.floatingPosition.Top)
+                {
+                    transform.position += Vector3.back * Time.deltaTime * speed;
+
+
+                }
+                else if (ef.lenNumber == EnemyFloating.floatingPosition.Bottom)
+                {
+                    transform.position += Vector3.forward * Time.deltaTime * speed;
+                }
+            }
+        }
+        
+    }
+}
--- Assets/EnemyFloating.cs
+++ Assets/EnemyFloating.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class EnemyFloating : MonoBehaviour
 {
+    public floatingPosition lenNumber;
     public Transform PlayerFloating;
     public float speed = 3;
     // Start is called before the first frame update
@@ -12,18 +13,30 @@ public class EnemyFloating : MonoBehaviour
         
     }
 
+    public bool reachTarget = false;
     // Update is called once per frame
     void Update()
     {
-        float dist = transform.position.x - PlayerFloating.position.x;
-        if (Mathf.Abs(dist) < 0.1f)
+        if (reachTarget == false)
         {
-            transform.position = new Vector3(PlayerFloating.position.x, transform.position.y, transform.position.z);
+            float dist = transform.position.x - PlayerFloating.position.x;
+            if (Mathf.Abs(dist) < 0.1f)
+            {
+                reachTarget = true;
+                transform.position = new Vector3(PlayerFloating.position.x, transform.position.y, transform.position.z);
+            }
+            else
+            {
+                transform.Translate(Vector3.right * speed * Time.deltaTime);
+            }
         }
-        else
-        {
-            transform.Translate(Vector3.right * speed * Time.deltaTime);
-        }
+        
        
+    }
+
+    public enum floatingPosition
+    {
+        Top,
+        Bottom
     }
 }
