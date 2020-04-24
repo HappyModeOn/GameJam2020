a70d94f9a2472db4dc3a0b35a267c3f796daad58
Tinnapop Sonporm <tinnapop.s@fairplaystudios.net>
2020-04-24 13:52:08 +0700

    Break Enemy Ship

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -9,6 +9,9 @@ public class EnemyController : MonoBehaviour
     // Start is called before the first frame update
     public bool isMelee = false;
 
+    public int hp = 3;
+    public int currentHP = 3;
+
     private Rigidbody rb;
     void Start()
     {
@@ -16,18 +19,38 @@ public class EnemyController : MonoBehaviour
         rb = gameObject.GetComponent<Rigidbody>();
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "HitBox")
+        {
+            currentHP -= 1;
+        }
+       
+    }
     // Update is called once per frame
+
+    public Vector3 knockbackPower;
+    public bool isDeath = false;
     void Update()
     {
         //rb.MovePosition(Vector3.forward * speed * Time.deltaTime);
-        
+        if (isDeath)
+        {
+            return;
+        }
+        if (currentHP <= 0)
+        {
+            transform.parent = null;
+            rb.AddForce(knockbackPower);
+            isDeath = true;
+        }
         if (ef.reachTarget)
         {
             if (isMelee)
             {
                 if (ef.lenNumber == EnemyFloating.floatingPosition.Top)
                 {
-                    transform.position += Vector3.back * Time.deltaTime * speed;
+                    transform.position += Vector3.back * Time.deltaTime * (speed - Random.Range(0,speed-1));
 
 
                 }
--- Assets/EnemyFloating.cs
+++ Assets/EnemyFloating.cs
@@ -4,19 +4,59 @@ using UnityEngine;
 
 public class EnemyFloating : MonoBehaviour
 {
+
+    public int hp = 10;
+    public int currentHP = 10;
+
+
     public floatingPosition lenNumber;
     public Transform PlayerFloating;
     public float speed = 3;
+
+    public List<EnemyController> ecs;
+    bool isBreak = false;
+    public float delayAfterBreak = 3;
     // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.name == "HitBox")
+        {
+            currentHP -= 1;
+        }
+        if (currentHP <= 0)
+        {
+            for (int i = 0; i < ecs.Count; i++)
+            {
+                if (ecs[i].isDeath == false)
+                {
+                    ecs[i].currentHP = -1;
+                }
+            }
+            isBreak = true;
+            transform.GetChild(0).gameObject.SetActive(false);
+            GetComponent<Rigidbody>().isKinematic = false;
+            GetComponent<Rigidbody>().AddForce(Random.Range(-300, 300), Random.Range(500, 1500) , 0);
+            GetComponent<Rigidbody>().useGravity = true;
+            GetComponent<Rigidbody>().AddExplosionForce(500, transform.position, 3);
+        }
     }
 
     public bool reachTarget = false;
     // Update is called once per frame
     void Update()
     {
+        if (isBreak)
+        {
+            if (delayAfterBreak > 0)
+            {
+                delayAfterBreak -= Time.deltaTime;
+            }
+            else
+            {
+                gameObject.SetActive(false);
+            }
+             
+        }
         if (reachTarget == false)
         {
             float dist = transform.position.x - PlayerFloating.position.x;
