ea94b357c061da8b08e6e0c5f130b16ff1195869
Tinnapop Sonporm <tinnapop.s@fairplaystudios.net>
2020-04-26 00:58:58 +0700

    no message

--- Assets/Floating.cs
+++ Assets/Floating.cs
@@ -60,6 +60,7 @@ public class Floating : MonoBehaviour
 
     IEnumerator DelayPlayerBGM()
     {
+       
         //yield on a new YieldInstruction that waits for 5 seconds.
         yield return new WaitForSeconds(7);
 
@@ -67,6 +68,8 @@ public class Floating : MonoBehaviour
         {
             PlayerFloating.GetComponent<AudioSource>().Play();
         }
+        yield return new WaitForSeconds(2);
+        topFloating.gameObject.SetActive(true);
     }
    
 public GameObject questPanel;
